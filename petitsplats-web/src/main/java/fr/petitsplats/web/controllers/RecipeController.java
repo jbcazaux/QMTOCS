16ad64588d4d109c8296f7e68f516dd8a6af45ab
jbcazaux <jbcazaux@gmail.com>
2012-04-08 13:20:25 +0200

    ajout du module core et du premier web service

---
+++ petitsplats-web/src/main/java/fr/petitsplats/web/controllers/RecipeController.java
@@ -0,0 +1,25 @@
+package fr.petitsplats.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fr.petitsplats.core.domain.Recipe;
+
+@Controller
+@RequestMapping(value = "/recipe")
+public class RecipeController {
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Recipe showIndex(@PathVariable int id) {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        recipe.setName("recipe " + id);
+
+        return recipe;
+    }
+}
