17438ab28a192daca16e7c0ff33a538e49f8126e
jbcazaux <jbcazaux@gmail.com>
2012-04-09 20:28:29 +0200

    met les relations de Recipe en Eager

--- petitsplats-web/src/main/java/fr/petitsplats/web/controllers/RecipeController.java
+++ petitsplats-web/src/main/java/fr/petitsplats/web/controllers/RecipeController.java
@@ -1,12 +1,16 @@
 package fr.petitsplats.web.controllers;
 
+import java.util.Collections;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import fr.petitsplats.domain.Ingredient;
 import fr.petitsplats.domain.Recipe;
+import fr.petitsplats.domain.RecipeStep;
 
 @Controller
 @RequestMapping(value = "/recipe")
@@ -16,9 +20,23 @@ public class RecipeController {
     @ResponseBody
     public Recipe showIndex(@PathVariable int id) {
 
+        // FIXME unmock !
+
+        Ingredient jambon = new Ingredient();
+        jambon.setName("jambon");
+
+        RecipeStep cuisson = new RecipeStep("faire cuire les pates");
+        RecipeStep pret = new RecipeStep("c'est pret");
+
         Recipe recipe = new Recipe();
+
+        recipe = new Recipe();
+        recipe.setTitle("jambon-pate");
+        recipe.setImageId(12);
+        recipe.setIngredients(Collections.singletonList(jambon));
+        recipe.addRecipeStep(cuisson);
+        recipe.addRecipeStep(pret);
         recipe.setId(id);
-        recipe.setTitle("recipe " + id);
 
         return recipe;
     }
