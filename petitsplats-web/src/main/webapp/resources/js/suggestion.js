b7c61aaa41ebcd3520e1f4623a42cd4c3f9124ca
jbcazaux <jbcazaux@gmail.com>
2012-05-09 22:58:09 +0200

    gestion du touch pour les device le supportant

--- petitsplats-web/src/main/webapp/resources/js/suggestion.js
+++ petitsplats-web/src/main/webapp/resources/js/suggestion.js
@@ -1,12 +1,17 @@
+
+function goToRecipe(id, reverse){
+	if ($('#suggestion_' + (id)).length > 0){
+		console.log('change page to : suggestion_' + (id));
+		$.mobile.changePage($('#suggestion_' + (id)), {reverse: reverse});	
+	}
+}
+
 $( ".prev" ).live('click', function(e){
 	
 	var page = $(e.target).parents(":jqmData(role='page')");
 	var id = parseInt(page.attr('id').split("_").pop());
 	
-	if ($('#suggestion_' + (id+1)).length > 0){
-		console.log('change page to : suggestion_' + (id+1));
-		$.mobile.changePage($('#suggestion_' + (id+1)), {reverse: true});	
-	}
+	goToRecipe(id + 1, true);
 	
 	return false;
 	
@@ -17,10 +22,7 @@ $( ".next" ).live('click', function(e){
 	var page = $(e.target).parents(":jqmData(role='page')");
 	var id = parseInt(page.attr('id').split("_").pop());
 	
-	if ($('#suggestion_' + (id-1)).length > 0){
-		console.log('change page to : suggestion_' + (id-1));
-		$.mobile.changePage($('#suggestion_' + (id-1)));	
-	}
+	goToRecipe(id - 1, false);
 	
 	
 	return false;
@@ -48,5 +50,23 @@ $( ":jqmData(role='page')" ).live( "pageshow", function(event) {
 	}
 	if (id > 1 && $('body').find('#suggestion_' + (id-1) ).length == 0){
 		retrievePage(id - 1);
+	}
+	
+	
+	if('ontouchstart' in document.documentElement){
+		$page.bind("swipeleft", function(){
+			goToRecipe(id - 1, false);
+		});
+		$page.bind("swiperight", function(){
+			goToRecipe(id + 1, true);
+		});
+//		$page.find( ".scrollable" ).first().bind("swipeleft", function(){
+//			console.log('swipe left');
+//			goToRecipe(id - 1, false);
+//		});
+//		$page.find( ".scrollable" ).first().bind("swiperight", function(){
+//			console.log('swipe right');
+//			goToRecipe(id + 1, true);
+//		});
 	}
 });
